c9a1bce0913b2552f282dfcd34241912ac554b24
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-12-18 12:19:44 +0900

    server posts model user 정보 추가

--- toto-list-server/src/api/posts/index.js
+++ toto-list-server/src/api/posts/index.js
@@ -1,12 +1,26 @@
+import checkLoggedIn from '../../lib/checkLoggedIn';
+
 const Router = require('koa-router');
 const postsCtrl = require('./posts.ctrl');
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
-posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
-posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+posts.post('/', checkLoggedIn, postsCtrl.write);
+posts.get('/:id', postsCtrl.getPostById, postsCtrl.read);
+posts.delete(
+  '/:id',
+  checkLoggedIn,
+  postsCtrl.getPostById,
+  postsCtrl.checkOwnPost,
+  postsCtrl.remove,
+);
+posts.patch(
+  '/:id',
+  checkLoggedIn,
+  postsCtrl.getPostById,
+  postsCtrl.checkOwnPost,
+  postsCtrl.update,
+);
 
 export default posts;
