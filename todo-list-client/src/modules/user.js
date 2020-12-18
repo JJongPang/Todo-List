d91567f8e721c5751e3a79070944d595fffa6a34
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-12-18 22:01:35 +0900

    로그인 검증 실패 시 정보 초기화

--- todo-list-client/src/modules/user.js
+++ todo-list-client/src/modules/user.js
@@ -5,7 +5,7 @@ import createRequestSaga, {
 import * as authAPI from '../lib/api/auth';
 import { takeLatest } from 'redux-saga/effects';
 
-const TEMP_SET_USER = 'user/TEMP_SET_USER';
+const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그인 처리
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
   'user/CHECK',
 );
@@ -18,11 +18,14 @@ const checkSaga = createRequestSaga(CHECK, authAPI.check);
 function checkFailureSaga() {
   try {
     localStorage.removeItem('user'); // localStorage에서 user 제거
+  } catch (e) {
+    console.log('localStorage is not working');
   }
 }
 
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
+  yield takeLatest(CHECK_FAILURE, checkFailureSaga);
 }
 
 const initialState = {
